31513c09062363c0ff7fd442241fb3a68518af95
safayet404 <hossainsafayet187@gmail.com>
2025-03-12 17:07:19 +0600

    cors handled

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const dbConnection = require('./utils/connectDB')
+const cors = require('cors');
 const app = express()
 const port = 5000
 
 dbConnection()
+app.use(cors())
 app.use(bodyParser.json())
-
 const religionRoutes = require('./routes/religionRoutes')
 const casteRoutes = require('./routes/casteRoutes')
 
